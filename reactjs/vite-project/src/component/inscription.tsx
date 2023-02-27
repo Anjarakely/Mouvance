3cd359684ef76465dd202fe8aedaf6601d370d0d
haja13 <rajerisonkarter98@gmail.com>
2023-02-27 14:36:31 +0100

    deuxieme modification

--- reactjs/vite-project/src/component/inscription.tsx
+++ reactjs/vite-project/src/component/inscription.tsx
@@ -1,15 +1,32 @@
-import React from "react" ; 
+import React, { ChangeEvent, useState } from "react" ; 
 import "../style/formulaire.css";
 import photo from"../image/undraw_education_f8ru.svg";
+
 
 const choixEtuC = "college";
 const choixEtul = "lycée";
 const choixEtuU = "université";
 
 const reponse = "";
+interface Inscription {
+    nom:string ,
+    prenom: string,
+    email:string,
+    mdp: string,
+    classe:[]  
+}
 
 
 const Inscription = () => {
+    const[data,useData] = useState<Inscription>({
+        nom : "",
+        prenom :"",
+        email:"",
+        mdp:"",
+        classe:[]
+    
+
+    })
     return(
         <div className="global-form">
              <div className="photo">
@@ -22,32 +39,77 @@ const Inscription = () => {
                     <form className="form">
                         <div className="input-box">
 
-                            <input type="text" />
+                            <input type="text" 
+                                onChange={
+                                    (e:ChangeEvent<HTMLInputElement>) =>{
+                                        useData({
+                                            ...data,
+                                            nom:e.target.value
+                                        });                                        
+                                    }
+                                }
+                            />
                             <label>Nom</label>
                         </div>
                         <div className="input-box">
-                            <input type="text" />
+                            <input type="text" 
+                                  onChange={
+                                    (e:ChangeEvent<HTMLInputElement>) =>{
+                                        useData({
+                                            ...data,
+                                            prenom:e.target.value
+                                        });
+
+                                    }
+                                }
+                            />
                             <label>Prenom</label>
                         </div>
                         <div className="input-box">
-                            <input type="text" />
+                            <input type="text" 
+                                  onChange={
+                                    (e:ChangeEvent<HTMLInputElement>) =>{
+                                        useData({
+                                            ...data,
+                                            email:e.target.value
+                                        });
+
+                                    }
+                                }
+                            
+                            />
                             <label>Email</label>
                         </div>
                         <div className="input-box">
-                            <input type="text" />
-                            <label>Mot de Passe </label>
+                            <input type="password" 
+                                  onChange={
+                                    (e:ChangeEvent<HTMLInputElement>) =>{
+                                        useData({
+                                            ...data,
+                                            mdp:e.target.value
+                                        });
 
-                        </div>
-                        <div className="input-box">
-                            <input type="text" />
+                                    }
+                                }
+                            />
                             <label>Mot de Passe </label>
 
                         </div>
                         
+                        
                         <div className="choix">
                             <div>
                                 <label> Classe d'étude </label>
-                                <select>
+                                <select  name="classe"
+                                    // onChange={
+                                    //     (e:ChangeEvent<HTMLSelectElement>) =>{
+                                    //         useData({
+                                    //             ...data,
+                                    //             classe:e.target.value
+                                    //         });
+                                    //     }
+                                    // }
+                                >
                                     <option value="college">collège</option>
                                     <option value="lycée">lycée</option>
                                     <option value="université">université</option>
@@ -57,7 +119,13 @@ const Inscription = () => {
                             <div>
                             <label> Classe d'étude </label>
 
-                            <select>
+                            <select
+                                 onChange={
+                                    (e:ChangeEvent<HTMLSelectElement>) =>{
+                                        console.log(e.target.value);
+                                    }
+                                }
+                            >
                                 <option value="">6ème</option>
                                 <option value="">5ème</option>
                                 <option value="">4ème</option>
@@ -67,12 +135,29 @@ const Inscription = () => {
                             <div>
                             <label> Classe d'étude </label>
 
-                            <select>
+                            <select
+                                 onChange={
+                                    (e:ChangeEvent<HTMLSelectElement>) =>{
+                                        console.log(e.target.value);
+                                    }
+                                }
+                            >
                                 <option value="">2nd</option>
                                 <option value="">1ère</option>
                                 <option value="">terminal</option>
                                 </select>
                             </div>
+                        </div>
+                        <div className="validation">
+                            <button
+                                // onClick={
+                                //     () =>{
+                                //         console.log(data)
+                                //     }
+                                // }
+                            >
+                                valider
+                            </button>
                         </div>
                     
 
